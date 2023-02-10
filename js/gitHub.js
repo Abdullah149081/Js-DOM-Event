17327f546dd48a7820a2ab4bed72ee1ed837d6d0
Abdullah149081 <masudmd425@gmail.com>
2023-02-10 21:18:22 +0600

    git Hub

---
+++ js/gitHub.js
@@ -0,0 +1,31 @@
+document.getElementById("btn-update").addEventListener("click", function () {
+  const inputUpdate = document.getElementById("input-update");
+  const inputText = inputUpdate.value;
+
+  const TextUpdate = document.getElementById("text-remove");
+
+  TextUpdate.innerText = inputText;
+  inputUpdate.value = "";
+});
+
+// btn display none
+document.getElementById("btn-delete").addEventListener("click", function () {
+  const textRemove = document.getElementById("text-remove");
+  textRemove.style.display = "none";
+});
+
+//   input text confirm
+
+document.getElementById("input-text").addEventListener("keyup", function (event) {
+  // input keyup type character
+  const text = event.target.value;
+
+  // btn
+  const btnConfirm = document.getElementById("btn-delete");
+
+  if (text === "delete") {
+    btnConfirm.removeAttribute("disabled");
+  } else {
+    btnConfirm.setAttribute("disabled", true);
+  }
+});
